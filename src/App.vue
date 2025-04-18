aa4e22f7ddf673749bb1bde8c205d5d39806c9ab
Nikita Kurbatow <mtobi3911@gmail.com>
2025-04-18 16:54:00 +0300

    Успешно исправил ошибки запуска проекта МММ класс

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,7 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>Не работает!!!</div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script></script>
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
